85fc2a2c39dde71c257b146573ee5585ebaf0c34
F9oF9o <sombun.than@hotmail.com>
2023-02-18 22:12:45 +0700

    COmmit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-dvdssdvdsvsvsdfv
+#pseudo-code
+#ตัวแปรที่ระบุสถานะของเกม
+score = 0
+lives = 3
+words = ['tone','Father Gumpon','Brother Jeep','My wife Ja']
+
+#ตราบใจที่ยังมีคำหายทายอยู่
+while(ยังมีคำให้ทาย) and (ชีวิตยังเหลือ):
+    #สุ่มคำจาก words แล้วดึงคำนั้นออกจาก list
+    secret_word = คำสุ่ม
+    clue = ['?','?'...]
