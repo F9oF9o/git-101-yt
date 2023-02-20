a4ab0b58312201afdf02742e82e9d594d55dffd9
F9oF9o <125373583+F9oF9o@users.noreply.github.com>
2023-02-20 10:13:17 +0700

    Commit

--- FirstGame.py
+++ FirstGame.py
@@ -1,1 +1,12 @@
-TabErrorS
+import random
+
+score = 0
+lives    = 3
+words = ['Tone', 'Ja','Jinny','Jom','Jeep']
+
+while (len(words)>0) and (lives>0):
+    random.shuffle(words)
+    secret_word = words.pop()
+    clue = list('?'*len(secret_word))
+
+  
