eb958328fbb67ad3b76e5e0c3d97a4497c199cf0
F9oF9o <125373583+F9oF9o@users.noreply.github.com>
2023-02-21 11:42:09 +0700

    Selenium.getElementByXpath

---
+++ Selenium.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import os
+import time
+path = os.getcwd()
+print(path)
+
+driver = webdriver.Edge(path+"/msedgedriver")
+driver.get("https://infoeasy.cc:9015/qr?id=EizwU%2fVQhPbtMZX7k%2fTV3aV1p0RB7nft83tnPZHzIwCimU7mGva0%2bgUHoPkuXDRK&orderid=NvE0s7VJZLI%3d&name=9CfMgg8c6L%2fQjfYdYdnT50K4HDhHiH6zK3sa9Xe0G%2b8%3d")
+
+
+# choiceitems =
+steak =  driver.find_element(By.XPATH, '//*[@id="menu-nav-list "]/li[2]/a')
+
+steak.click()
+  
+  
+
+time.sleep(15)
