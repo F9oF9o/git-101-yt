2509bd2fe06dcb4b285799335dba40d56e5e252b
F9oF9o <125373583+F9oF9o@users.noreply.github.com>
2023-02-24 16:13:58 +0700

    Commit update selenium 20230224 time 1613

--- Selenium.py
+++ Selenium.py
@@ -7,9 +7,9 @@ import time
 
 driver = webdriver.Edge()
 driver.get('https://infoeasy.cc:8083/qr?id=EizwU%2fVQhPbtMZX7k%2fTV3VtQnv9EVX3E6UD06jytjxweEhclducIHIKdA%2bfmM4JP&orderid=afGbNmqB96c%3d&name=46pXnExyHESvKoNzowd7bUKrWWcBqH%2bnYM%2bMLCY2G5A%3d')
-time.sleep(3)
-
+driver.maximize_window()
 remark = 'ไม่ใส่อะไรทั้งสิ้น'
+time.sleep(3)
 # choiceitems  1
 firstitems =  driver.find_element(By.XPATH, '//*[@id="menu-82"]/div/div/a[9]')
 firstitems.click()
@@ -18,30 +18,27 @@ time.sleep(3)
 clickplus = driver.find_element(By.XPATH, '//*[@id="item-modifier"]/div[3]/div[1]/div/div/div[1]').click()
 # clickplus.double_Click()
 # Comment 1
-sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]')
-sendtext.send_keys(remark)
+sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]').send_keys(remark)
 # NextPage 1
 time.sleep(1)
-nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]')
-nextpage.click()
+nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]').click()
 time.sleep(3)
 #---------------------------------------------------------------------
 # loop ChooseItems
 # choiceitems  2
-firstitems =  driver.find_element(By.XPATH, '//*[@id="menu-82"]/div/div/a[5]')
-firstitems.click()
+firstitems =  driver.find_element(By.XPATH, '//*[@id="menu-82"]/div/div/a[5]').click()
 time.sleep(3)
 # Click +2
-clickplus = driver.find_element(By.XPATH, '//*[@id="item-modifier"]/div[3]/div[1]/div/div/div[1]')
-clickplus.click()
+clickplus = driver.find_element(By.XPATH, '//*[@id="item-modifier"]/div[3]/div[1]/div/div/div[1]').click()
 # Comment 2
-sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]')
-sendtext.send_keys(remark)
+sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]').send_keys(remark)
 # NextPage 2
-nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]')
-nextpage.click()
+nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]').click()
 time.sleep(1)
 # ClickCart
-clickcart = driver.find_element(By.XPATH,'//*[@id="cart-btn"]')
-clickplus.click()
+clickcart = driver.find_element(By.XPATH,'//*[@id="cart-btn"]').click()
+time.sleep(1)
+nextpage_2 = driver.find_element(By.XPATH,'//*[@id="add-cart"]').click()
+time.sleep(1)
+click_2 = driver.find_element(By.XPATH,'//*[@id="confirm-order-btn"]').click()
 time.sleep(15)
