777e0ff86403250ac382b0837fcc10c6ee631e1d
F9oF9o <125373583+F9oF9o@users.noreply.github.com>
2023-02-24 15:54:03 +0700

    new file selenium

--- Selenium.py
+++ Selenium.py
@@ -6,13 +6,42 @@ import time
 
 
 driver = webdriver.Edge()
-driver.get("https://infoeasy.cc:9015/qr?id=EizwU%2fVQhPbtMZX7k%2fTV3aV1p0RB7nft83tnPZHzIwCimU7mGva0%2bgUHoPkuXDRK&orderid=NvE0s7VJZLI%3d&name=9CfMgg8c6L%2fQjfYdYdnT50K4HDhHiH6zK3sa9Xe0G%2b8%3d")
+driver.get('https://infoeasy.cc:8083/qr?id=EizwU%2fVQhPbtMZX7k%2fTV3VtQnv9EVX3E6UD06jytjxweEhclducIHIKdA%2bfmM4JP&orderid=afGbNmqB96c%3d&name=46pXnExyHESvKoNzowd7bUKrWWcBqH%2bnYM%2bMLCY2G5A%3d')
 time.sleep(3)
 
-# choiceitems =
-steak =  driver.find_element(By.XPATH, '//*[@id="menu-275"]/div/div/a[2]')
-steak.click()
+remark = 'ไม่ใส่อะไรทั้งสิ้น'
+# choiceitems  1
+firstitems =  driver.find_element(By.XPATH, '//*[@id="menu-82"]/div/div/a[9]')
+firstitems.click()
 time.sleep(3)
-  
-
+# Click + 1
+clickplus = driver.find_element(By.XPATH, '//*[@id="item-modifier"]/div[3]/div[1]/div/div/div[1]').click()
+# clickplus.double_Click()
+# Comment 1
+sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]')
+sendtext.send_keys(remark)
+# NextPage 1
+time.sleep(1)
+nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]')
+nextpage.click()
+time.sleep(3)
+#---------------------------------------------------------------------
+# loop ChooseItems
+# choiceitems  2
+firstitems =  driver.find_element(By.XPATH, '//*[@id="menu-82"]/div/div/a[5]')
+firstitems.click()
+time.sleep(3)
+# Click +2
+clickplus = driver.find_element(By.XPATH, '//*[@id="item-modifier"]/div[3]/div[1]/div/div/div[1]')
+clickplus.click()
+# Comment 2
+sendtext = driver.find_element(By.XPATH,'//*[@id="remark"]')
+sendtext.send_keys(remark)
+# NextPage 2
+nextpage = driver.find_element(By.XPATH, '//*[@id="add-choice-btn"]')
+nextpage.click()
+time.sleep(1)
+# ClickCart
+clickcart = driver.find_element(By.XPATH,'//*[@id="cart-btn"]')
+clickplus.click()
 time.sleep(15)
