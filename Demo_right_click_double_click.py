da73cd6499b9f0797306f0addddd663f9d6aaf42
F9oF9o <125373583+F9oF9o@users.noreply.github.com>
2023-02-24 16:03:30 +0700

    Commit

---
+++ Demo_right_click_double_click.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+import time 
+
+driver = webdriver.Edge()
+driver.get('https://infoeasy.cc:8083/qr?id=EizwU%2fVQhPbtMZX7k%2fTV3VtQnv9EVX3E6UD06jytjxweEhclducIHIKdA%2bfmM4JP&orderid=afGbNmqB96c%3d&name=46pXnExyHESvKoNzowd7bUKrWWcBqH%2bnYM%2bMLCY2G5A%3d')
+time.sleep(3)
